f00de9d2646bb17f1cb4fee9914ae6f730732c70
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-17 16:46:11 -0600

    Load the .JSON file

---
+++ messengerAnalyzer.js
@@ -0,0 +1,7 @@
+//This field should include the .json file extension.
+let fileName = ''
+
+let messengerData = require(`./${fileName}`);
+
+//Logs the data from the .json file.
+console.log(messengerData)
