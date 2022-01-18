9855a64014ed578a567b042fcdcca9467d78556f
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-18 16:24:58 -0600

    Add logic to not add messages only containing links or reactions

--- messengerAnalyzer.js
+++ messengerAnalyzer.js
@@ -1,7 +1,9 @@
-// //Next to do is figure out how to break the messages into words at spaces, \n, and special characters in order to add them to wordsArray
-// //add optional paramaters for words to skip and date ranges
-// //skip message content that begins with Reacted
-// //how to deal with undefined?
+//add optional paramaters for words to skip and date ranges
+//add logic to count messages per day average
+//add logic to parse word lists and count the most used words
+//add all below into a function with parameters "dateStart", "dateEnd", "wordsToOmit" (which is an array of common words like the, a, an and will be used when looping the final words array and counting words "if not in wordsToOmit")
+//counts words of both of us individually and together
+//does it work for convos with multiple people?
 
 
 //Creates an array of all file names in the messages directory
@@ -18,28 +20,33 @@ fileNames.forEach(file => {
 
 
 
-//loops through the filesObject
-//add logic to remove messages that only contain media and do not contain text (Should be available on main branch using &&)
-//add logic to not include strings starting with Reacted 
-//add logic to strip message['content'] using regex
+//loops through the filesObject and creates messagesObject which contains only the desired data from all JSON filesObject
+//Removes messages with no text i.e. only media files, reactions, or links
+//add logic to add an array of words to the end of the object in the array {text : message['content'], date_ms : message['timestamp_ms', words: xx]}. keep text so that I can calculate things like messages per day?
 messagesObject = {};
 for(let file in filesObject){
     for(let message of filesObject[file]['messages']){
-        if(messagesObject[message['sender_name']] === undefined && message['content'] !== undefined){
-            messagesObject[message['sender_name']] = [{text : message['content'], date_ms : message['timestamp_ms']}]
-        } else if(message['content'] !== undefined){
-            messagesObject[message['sender_name']] = messagesObject[message['sender_name']].concat([{text : message['content'], date_ms : message['timestamp_ms']}])
+        if(messagesObject[message['sender_name']] === undefined && message['content'] !== undefined && /^(?!Reacted|https)/.test(message['content'])){
+            messagesObject[message['sender_name']] = [{text: message['content'], date_ms: message['timestamp_ms']}]
+        } else if(message['content'] !== undefined && /^(?!Reacted|https)/.test(message['content'])){
+            messagesObject[message['sender_name']] = messagesObject[message['sender_name']].concat([{text: message['content'], date_ms : message['timestamp_ms']}])
 
         }
     }
 }
-
 
 console.dir(messagesObject, { depth: null })
 
 
 
 
+
+
+// //Use this to add arrays of the words in the messages
+// let test = "this*is " + " a\n" +" test string "
+// test = test.trim().split(/\W+/g)
+// // test = test.split(/\W/)g.filter(word => word !== '')
+// console.log(test)
 
 
 
@@ -80,9 +87,7 @@ console.dir(messagesObject, { depth: null })
 // // console.log (wordsArray)
 // // console.log(messagesArray)
 
-// let test = 'this*is ' + "a\n" +" test"
-// test = test.split(/\W/)
-// console.log(test)
+
 
 
 
