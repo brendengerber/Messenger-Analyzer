fb6c69bc241cb46477283d7242e573cafd320678
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-18 14:20:33 -0600

    Add logic to loop through all files and parse them

--- messengerAnalyzer.js
+++ messengerAnalyzer.js
@@ -15,46 +15,75 @@ fileNames.forEach(file => {
     filesObject[file] = require("./messages/"+file);
 })
 
-// console.log(filesObject['message_1.json']['messages'])
 
 
 
-
-
-//loops through the filesObject which contains the objects from each JSON file and puts the messages into an array
-//removes messages that only contain media and do not contain text
-messagesArray = [];
+//loops through the filesObject
+//add logic to remove messages that only contain media and do not contain text (Should be available on main branch using &&)
+//add logic to not include strings starting with Reacted 
+//add logic to strip message['content'] using regex
+messagesObject = {};
 for(let file in filesObject){
-    filesObject[file]['messages'].forEach(message => {
-        if(message['content'] !== undefined){
-            messagesArray.push(message['content'])
+    for(let message of filesObject[file]['messages']){
+        if(messagesObject[message['sender_name']] === undefined){
+            messagesObject[message['sender_name']] = [{text : message['content'], date_ms : message['timestamp_ms']}]
+        } else {
+            messagesObject[message['sender_name']] = messagesObject[message['sender_name']].concat([{text : message['content'], date_ms : message['timestamp_ms']}])
+
         }
-    })
-}
-
-
-//foreach method, but not working due to formatting?
-// filesObject.forEach(file => {
-//     filesObject[file]['messages'].forEach(message => {
-//         messagesArray.push(message['content'])
-//     })
-// })
-
-//breaks messagesArray into an array containing only one word per index, not working
-let wordsArray = []
-for(let messages in messagesArray){
-    for(let words in messages){
-        console.log(words)
-        wordsArray.push(words)
     }
 }
 
-// console.log (wordsArray)
-// console.log(messagesArray)
 
-let test = 'this*is ' + "a\n" +" test"
-test = test.split(/\W/)
-console.log(test)
+console.dir(messagesObject, { depth: null })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //foreach method, but not working due to formatting?
+// // filesObject.forEach(file => {
+// //     filesObject[file]['messages'].forEach(message => {
+// //         messagesArray.push(message['content'])
+// //     })
+// // })
+// console.log(messagesArray)
+// //breaks messagesArray into an array containing only one word per index, not working
+// let wordsArray = []
+// for(let messages in messagesArray){
+//     for(let words in messages){
+//         wordsArray.push(words)
+//     }
+// }
+
+// // console.log (wordsArray)
+// // console.log(messagesArray)
+
+// let test = 'this*is ' + "a\n" +" test"
+// test = test.split(/\W/)
+// console.log(test)
 
 
 
