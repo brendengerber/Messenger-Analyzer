81c9dfa5b1785adc0acd0361a281bcaf80cc340c
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-24 13:38:04 -0600

    Refactor JSON file creation and logData method

--- messengerAnalyzer.js
+++ messengerAnalyzer.js
@@ -5,29 +5,26 @@
 //add logic to count total messages sent for each sender
 //add all below into a function with parameters "dateStart", "dateEnd", "wordsToOmit" (which is an array of common words like the, a, an and will be used when looping the final words array and counting words "if not in wordsToOmit")
 //determine dataset date range and display a message if the range input is outside that range?
+//writeFile vs writeFileSync
 
 
 
 let messageAnalyzer = {
     parseFiles: function(){
-        const fs  = require('fs');
         let messagesData = {};
         //Regex to match current and retired Facebook emoticons as of 01/01/2022
         const emoticons = /O:-\)|O:\)|>:-\(|:42:|:-D|:D|:putnam:|O.o|:'\(|3:-\)|3:\)|=P|B\)|B-\)|8\)|8-\)|>:\(|=\)|<3|:-\/|:-\*|:\*|:3|(Y)|\^_\^|:v|<\("\)|:\|\]|=\(|:\[|:\(|:-\(|\(\^\^\^\)|:-o|:-O|:\]|:-\)|:\)|-_-|:-p|:-P|:p|:P|B\||B-\||8\||8-\||:-o|:-O|:o|:O|:\\|:-\\|:\/|>-:o|>:-O|=D|;-\)|:\)|>:o|>:O/g;
-
         //Create an array of all file names in the messages directory
         let messageDirectory = "./messages";
-        let fileNames = fs.readdirSync(messageDirectory);
-
+        let fileNames = require('fs').readdirSync(messageDirectory);
         //Create an object containing all objects from the JSON files
         let filesObject = {};
         fileNames.forEach(file => {
             filesObject[file] = require("./messages/"+file);
         });
-
         //Loop through filesObject and creates messagesData which contains only the desired data from all JSON files in filesObject
         //Remove messages with no text i.e. only media files(undefined), reactions(Reacted), links(https), or emoticons (both sequential or separated by spaces)
-        //Create an array for all the words seperately, while stripping them of special characters (except apostrophe), whitespace, newlines, and emoticons
+        //Create an array for all the words seperately, while stripping them of special characters (except apostrophe), whitespace, newlines, emoticons, and capital letters
         for(let file in filesObject){
             for(let message of filesObject[file]['messages']){
                 //Check if message is undefined, media, reaction, or contains only emoticons and continues if so
@@ -35,27 +32,36 @@ let messageAnalyzer = {
                     continue
                 //Add sender and message to messagesData if sender has not been added
                 }else if(messagesData[message['sender_name']] === undefined){
-                    messagesData[message['sender_name']] = [{date_ms: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().split(' ')}];
+                    messagesData[message['sender_name']] = [{date_ms: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().toLowerCase().split(' ')}];
                 //Add message to messagesData
                 }else{
-                    messagesData[message['sender_name']] = messagesData[message['sender_name']].concat([{date_ms: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().split(' ')}]);
+                    messagesData[message['sender_name']] = messagesData[message['sender_name']].concat([{date_ms: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().toLowerCase().split(' ')}]);
                 }
             }
         }
-        fs.writeFileSync("./messages/data.JSON", JSON.stringify(messagesData))
+        require('fs').writeFileSync("./data/data.json", JSON.stringify(messagesData));
     },
-    //change to access file not object
     logData: function(){
-        if(Object.keys(this.messageData).length === 0){
-            console.log('Data has not yet been parsed. Run "messengerAnalyzer.parseFiles()" and try again.');
-        }else{
-            console.dir(this.messageData, { depth: null });
+        if(!require('fs').existsSync("./data/data.json")){
+            this.parseFiles();
         }
+        console.dir(require("./data/data.json"),{ depth: null });
     },
+
+
+
+
+
+
+
+
+
+
+
     rankWords: function(){
         const fs = require("fs");
-        if(!fs.existsSync("./messages/data.JSON")){
-            this.parseFiles()
+        if(!fs.existsSync("./data/data.json")){
+            this.parseFiles();
         }
         //add rankWords function here
     },
@@ -69,7 +75,7 @@ let messageAnalyzer = {
     }
 }
 
-messageAnalyzer.parseFiles()
+messageAnalyzer.logData()
 
 
 // rankWords: function(data_file){
@@ -80,6 +86,7 @@ messageAnalyzer.parseFiles()
 //     //add function here to create the wordInstances and then organize it
 //     }
 // }
+
 
 //before all else have parse files create the JSON file, and put messenger data object inside that function
 //then change logData to check if the JSON data file exists rather than if the messenger data object has a length, maybe instead of an error just have it run it.
