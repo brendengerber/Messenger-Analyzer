e148910763f51ef80d23c6d6d9573fa0ce7d4d76
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-25 10:42:55 -0600

    Add beginning logic for rankWords method to rank but not yet order word instances

--- messengerAnalyzer.js
+++ messengerAnalyzer.js
@@ -6,10 +6,15 @@
 //add all below into a function with parameters "dateStart", "dateEnd", "wordsToOmit" (which is an array of common words like the, a, an and will be used when looping the final words array and counting words "if not in wordsToOmit")
 //determine dataset date range and display a message if the range input is outside that range?
 //writeFile vs writeFileSync
+//change double quotes to singles for consistency 
+
 
 
 
 let messageAnalyzer = {
+    //* this works almost, but check out the weird numbers when consol logging wordInstances in the rankWords method, why are they getting in there during parseFiles()?  some are coming as part of unicode? some are coming from links after text (such as 14791675), how can I remove those links when regex http trigers? It's important to remove them as they will influence daily and yearly message averages etc.
+    //* perhaps remove everything unwanted in the line inside the for loop but before the if statements. For example remove emoji, and urls (from everywhere not just beginning), and then the first if statement can be to continue if it's empty, undefined, or only reacted. Just have to make sure spaces don't slip in somehow. or maybe it can just go into each elseif since emoticons are already there etc.
+    //* for example replace all with a space and then trim. if they had only emoticons and links, then they will only be spaces and will trim to nothing and be skipped.
     parseFiles: function(){
         let messagesData = {};
         //Regex to match current and retired Facebook emoticons as of 01/01/2022
@@ -25,17 +30,18 @@ let messageAnalyzer = {
         //Loop through filesObject and creates messagesData which contains only the desired data from all JSON files in filesObject
         //Remove messages with no text i.e. only media files(undefined), reactions(Reacted), links(https), or emoticons (both sequential or separated by spaces)
         //Create an array for all the words seperately, while stripping them of special characters (except apostrophe), whitespace, newlines, emoticons, and capital letters
+        //*Should I loop through and add senders as empty objects first like i do in rankWords?
         for(let file in filesObject){
             for(let message of filesObject[file]['messages']){
                 //Check if message is undefined, media, reaction, or contains only emoticons and continues if so
-                if((message['content'] !== undefined && message['content'].replace(emoticons, '').replace(/\s+/,'') === '') || /^Reacted|https/.test(message['content']) || message['content'] === undefined){
+                if((message['content'] !== undefined && message['content'].replace(emoticons, '').replace(/\s+/,'') === '') || /^http|Reacted/.test(message['content']) || message['content'] === undefined){
                     continue
                 //Add sender and message to messagesData if sender has not been added
                 }else if(messagesData[message['sender_name']] === undefined){
-                    messagesData[message['sender_name']] = [{date_ms: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().toLowerCase().split(' ')}];
+                    messagesData[message['sender_name']] = [{dateMs: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().toLowerCase().split(' ')}];
                 //Add message to messagesData
                 }else{
-                    messagesData[message['sender_name']] = messagesData[message['sender_name']].concat([{date_ms: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().toLowerCase().split(' ')}]);
+                    messagesData[message['sender_name']] = messagesData[message['sender_name']].concat([{dateMs: message['timestamp_ms'], words: message['content'].replace(emoticons, ' ').replace(/(?!')\W+/g, ' ').trim().toLowerCase().split(' ')}]);
                 }
             }
         }
@@ -57,48 +63,67 @@ let messageAnalyzer = {
 
 
 
-
-    rankWords: function(){
-        const fs = require("fs");
-        if(!fs.existsSync("./data/data.json")){
+//* this is working (but needs to be ordered still...sort the object's keys by their value)
+//* user input needs to be converted to ms and it will work flawlessly
+//* need to add words to skip
+//* https://stackoverflow.com/questions/1069666/sorting-object-property-by-values
+    rankWords: function(startDate, endDate){
+        if(!require('fs').existsSync("./data/data.json")){
             this.parseFiles();
         }
-        //add rankWords function here
+        let messageData = require("./data/data.json");
+        let wordInstances = {};
+        let sortableWordInstances =[];
+        for(let sender in messageData){
+            wordInstances[sender]={};
+        }
+        for(let sender in messageData){
+            for(let message of messageData[sender]){
+                // if(message['dateMs'] >= startDate && message['dateMs' <= endDate]){
+                    for(let word of message['words']){
+                       if(word in wordInstances[sender]){
+                           wordInstances[sender][word] = wordInstances[sender][word] + 1;
+                       }else{
+                           wordInstances[sender][word] = 1
+                       }
+                    }
+                // }
+            }
+        }
+        console.log(wordInstances)
     },
+
+
+
+
+
+
+
+
+
+
     rankDays: function(){
-//add rankDays function here and return the result
+//*add rankDays function here and return the result
     },
     analyzeData: function(startDate, endDate, wordsToSkip){
         this.rankWords();
         this.rankDays();
-//Add all analysis functions here
+// *Add all analysis functions here
     }
 }
-
-messageAnalyzer.logData()
-
-
-// rankWords: function(data_file){
-//     if(dataJSON does not exist){
-//         create JSON using .parsefiles
-//     } else {
-//         let wordInstances = [{word: 'word1', totalNumber: 4}, {word: 'word2', totalNumber: 5}];
-//     //add function here to create the wordInstances and then organize it
-//     }
-// }
+messageAnalyzer.rankWords(0,100000000000000000000000)
 
 
-//before all else have parse files create the JSON file, and put messenger data object inside that function
-//then change logData to check if the JSON data file exists rather than if the messenger data object has a length, maybe instead of an error just have it run it.
+// //create a different method for each analysis and have analysis method call each one. Each method should return the data and to view it log the analysis method
+
+// // loop through if the word exists totalNumber ++ if not then add it. then organize it in another loop
+
+// // similar list of objects but for the days of the week. is the ms a monday? if so monday ++. Look up if there is a way to check day of the week for ms time.
+
+// //look up best way to organize an array by rank and apply it to my mini objects, by calculating the value of the object first and using that to sort
 
 
-//create a different method for each analysis and have analysis method call each one. Each method should return the data and to view it log the analysis method
-
-// loop through if the word exists totalNumber ++ if not then add it. then organize it in another loop
-
-// similar list of objects but for the days of the week. is the ms a monday? if so monday ++. Look up if there is a way to check day of the week for ms time.
-
-//look up best way to organize an array by rank and apply it to my mini objects, by calculating the value of the object first and using that to sort
+// //add An error has occured message to each function at the end of if else, as a catch all? or maybe it is just needed on the analyze file. Otherwise the data file either exists or it doesn't
 
 
-//add An error has occured message to each function at the end of if else, as a catch all? or maybe it is just needed on the analyze file. Otherwise the data file either exists or it doesn't
+
